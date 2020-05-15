3b21871a9e28047890cc603a5610669122349b7f
Adam Axelsson <adam.axelsson1992@gmail.com>
2020-05-15 13:28:48 +0200

    0.03 login works

--- src/JS/site.js
+++ src/JS/site.js
@@ -1,4 +1,6 @@
 GetMovies();
+ShowLoginOrLoggedIn();
+const mainDiv = document.getElementById("main");
 
 async function GetMovies() {
     try {
@@ -24,14 +26,69 @@ async function GetMovies() {
                         `<p>In stock: ${movie.stock}</p>` +
                     "</div>" +
                     "<div class=\"card-footer\">"+
-                    "<small class=\"text-muted\">Last updated x mins ago</small>"+
+                        `<a href='http://127.0.0.1:5500/src/Pages/trivia.html?movie=${movie.id}' class='button'>Trivia</a>`
+                        "<small class=\"text-muted\">Last updated x mins ago</small>"+
                     "</div>"
             "</div>"+
-
             movieDiv.insertAdjacentElement("beforeend", movieCard);
         });
+    } catch (error) {
+        console.log(error);
     }
-    catch (error) {
-        console.log(error)
+}
+
+function ShowLoginOrLoggedIn() {
+    if(localStorage.getItem("user") != null){
+        LoggedIn();
     }
+    else{
+        ShowLogin();
+    }
+}
+function ShowLogin(){
+    const loginDiv = document.getElementById("login");
+
+    loginDiv.innerHTML = 
+    '<div class="login-container">'+
+      '<input type="text" placeholder="Username" id="username">'+
+      '<input type="password" placeholder="Password" id="pw">'+
+      '<button type="submit" onclick="Login()">Login</button>'+
+     '</div>'
+}
+function LoggedIn(){
+    const loginDiv = document.getElementById("login");
+    loginDiv.innerHTML = "";
+
+    loginDiv.insertAdjacentHTML("afterbegin" , '<div class="login-container">'+
+    '<button type="submit" onclick="Logout()">Logout</button>'+
+    `<p>Logged in as ${localStorage.getItem("user")}</p>`+
+    '</div>');
+
+}
+async function Login(){
+    let user, password ="";
+    userName = document.getElementById("username").value;
+    pw = document.getElementById("pw").value
+    
+    try {
+        const response = await fetch("https://localhost:5001/api/filmstudio");
+        const data = await response.json();
+
+        data.forEach(user => {
+            if(user.password == pw && user.name == userName){
+                localStorage.setItem("user", user);
+                console.log("got here");
+            }
+        });
+        if(localStorage.getItem("user") == null){
+            console.log("wrong login")
+        }
+        ShowLoginOrLoggedIn();
+    } catch (error) {
+        console.log(error);
+    }
+}
+async function Logout() {
+    localStorage.removeItem("user");
+    ShowLogin();
 }
