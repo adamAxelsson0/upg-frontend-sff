39ffd29dc61a01ab9b50840cf511dd833359c40c
Adam Axelsson <adam.axelsson1992@gmail.com>
2020-05-18 12:04:59 +0200

    login displays correctly

--- src/JS/login.js
+++ src/JS/login.js
@@ -1,59 +1,60 @@
 ShowLoginOrLoggedIn();
 
 function ShowLoginOrLoggedIn() {
-    if(localStorage.getItem("user") != null){
+    if (localStorage.UserName != null) {
         LoggedIn();
     }
-    else{
+    else {
         ShowLogin();
     }
 }
-function ShowLogin(){
+function ShowLogin() {
     const loginDiv = document.getElementById("login");
     const registerdiv = document.getElementById("register");
 
-    loginDiv.innerHTML = 
-    '<div class="login-container">'+
-     `<button onclick="Register()">Register</button>` +
-         '<div id="modal-register" class="modal">' +
-         '<div class="modal-content">'+
-         '<div class="modal-header">'+
-           '<span class="close">&times;</span>'+
-           `<h2>Register account</h2>`+
-         '</div>'+
-         '<div id="modal-body-register" class="modal-body">'+
-            
-         '</div>'+
-       '</div>'+
-         '</div>'+
-      '<input type="text" placeholder="Username" id="username">'+
-      '<input type="password" placeholder="Password" id="pw">'+
-      '<button type="submit" onclick="Login()">Login</button>'+
-     '</div>';
+    loginDiv.innerHTML =
+        '<div class="login-container">' +
+        `<button onclick="Register()">Register</button>` +
+        '<div id="modal-register" class="modal">' +
+        '<div class="modal-content">' +
+        '<div class="modal-header">' +
+        '<span class="close">&times;</span>' +
+        `<h2>Register account</h2>` +
+        '</div>' +
+        '<div id="modal-body-register" class="modal-body">' +
+
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '<input type="text" placeholder="Username" id="username">' +
+        '<input type="password" placeholder="Password" id="pw">' +
+        '<button type="submit" onclick="Login()">Login</button>' +
+        '</div>';
 }
-function LoggedIn(){
+function LoggedIn() {
     const loginDiv = document.getElementById("login");
-    user = localStorage.getItem('user');
-    loginDiv.innerHTML = '<div class="login-container">'+
-    '<button type="submit" onclick="Logout()">Logout</button>'+
-    `<p>Logged in as ${user.name}</p>`+
-    '</div>';
+    userName = localStorage.UserName;
+    loginDiv.innerHTML = '<div class="login-container">' +
+        '<button type="submit" onclick="Logout()">Logout</button>' +
+        `<p>Logged in as ${userName}</p>` +
+        '</div>';
 }
-async function Login(){
-    let user, password ="";
+async function Login() {
+    let user, password = "";
     userName = document.getElementById("username").value;
     pw = document.getElementById("pw").value
-    
+
     try {
         const response = await fetch("https://localhost:5001/api/filmstudio");
         const data = await response.json();
 
         data.forEach(user => {
-            if(user.password == pw && user.name == userName && user.verified == true){
-                localStorage.setItem("user", user);
+            if (user.password == pw && user.name == userName && user.verified == true) {
+                localStorage.UserName = user.name;
+                localStorage.Id = user.id;
             }
         });
-        if(localStorage.getItem("user") == null){
+        if (localStorage.UserName == null) {
             console.log("wrong login")
         }
         ShowLoginOrLoggedIn();
@@ -62,7 +63,7 @@ async function Login(){
     }
 }
 async function Logout() {
-    localStorage.removeItem("user");
+    localStorage.clear();
     ShowLogin();
 }
 
@@ -72,41 +73,34 @@ async function Register() {
     var span = document.getElementsByClassName("close")[0];
     const registerBody = document.getElementById("modal-body-register");
 
-    registerBody.innerHTML = '<input type="text" placeholder="Enter Username" id="uname" required>'+
-    '<input type="password" placeholder="Enter Password" id="psw" required>'+
-    '<button type="submit" onclick="TryRegister()">Register</button>';
+    registerBody.innerHTML = '<form id="registerForm">' +
+        '<label for="userName">Name of Studio:</label>' +
+        '<input type="text" id="userName" name="name">' +
+        '<br>' +
+        '<label for="pw">Password:</label>' +
+        '<input type="password" id="pw" name="password">' +
+        '<br>' +
+        '<button type="submit" onclick="TryRegister()">Register</button>' +
+        '</form>';
 
     modal.style.display = "block";
 
-    span.onclick = function() {
-    modal.style.display = "none";
-  }
-  window.onclick = function(event) {
-      if (event.target == modal) {
+    span.onclick = function () {
         modal.style.display = "none";
-      }
+    }
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
     }
 }
-async function TryRegister(){
-    var userName = document.getElementById("uname").value;
-    var passWord = document.getElementById("psw").value;
-    var filmstudio =  { name: userName, password: passWord, verified: false};
-    try {
-        let response = await fetch('https://localhost:5001/api/filmstudio', {
+async function TryRegister() {
+        const thisForm = document.getElementById('registerForm');
+        const formData = new FormData(thisForm).entries()
+        const response = await fetch('https://localhost:5001/api/filmstudio', {
             method: 'POST',
-            headers: {
-              'Content-Type': 'application/json;charset=utf-8'
-            },
-            body: JSON.stringify(filmstudio)
-          });
-          
-          let result = await response.json();
-          alert(result.name + " has been registered and is waiting approval.");
-
-        const registerBody = document.getElementById("modal-body-register");
-        registerBody.innerHTML = "<p>Your account is waiting approval</p>";
-
-    } catch (error) {
-        
-    }
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(Object.fromEntries(formData))
+        });
+        alert("Your application has been registered. Please wait for verification. Standard verification handling time is X hours/days.");
 }
